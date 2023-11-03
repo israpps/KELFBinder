c36e6959ccd65a916b135e428ac730362e216bd2
israpps <57065102+israpps@users.noreply.github.com>
2023-11-03 14:50:22 -0300

    add atpanic()

--- src/luaplayer.cpp
+++ src/luaplayer.cpp
@@ -9,7 +9,53 @@
 #include "include/dbgprintf.h"
 
 static lua_State *L;
+#define DUAL_PRINTF(x...) scr_printf(x); printf(x)
+int test_error(lua_State * L) {
+    int n = lua_gettop(L);
+    int i;
 
+        scr_setfontcolor(0x0000ff);
+        scr_setCursor(0);
+        scr_clear();
+    printf("ERROR.\n");
+
+    if (n == 0) {
+        DUAL_PRINTF("Stack is empty.\n");
+    }
+
+    for (i = 1; i <= n; i++) {
+        printf("%i: ", i);
+        switch(lua_type(L, i)) {
+        case LUA_TNONE:
+            DUAL_PRINTF("Invalid");
+            break;
+        case LUA_TNIL:
+            DUAL_PRINTF("(Nil)");
+            break;
+        case LUA_TNUMBER:
+            DUAL_PRINTF("(Number) %f", lua_tonumber(L, i));
+            break;
+        case LUA_TBOOLEAN:
+            DUAL_PRINTF("(Bool)   %s", (lua_toboolean(L, i) ? "true" : "false"));
+            break;
+        case LUA_TSTRING:
+            DUAL_PRINTF(" %s", lua_tostring(L, i));
+            break;
+        case LUA_TTABLE:
+            DUAL_PRINTF("(Table)");
+            break;
+        case LUA_TFUNCTION:
+            DUAL_PRINTF("(Function)");
+            break;
+        default:
+            DUAL_PRINTF("<UNKNOWN>");
+        }
+
+        DUAL_PRINTF("\n");
+    }
+
+    SleepThread();
+}
 const char * runScript(const char* script, bool isStringBuffer )
 {
     DPRINTF("Creating luaVM... \n");
@@ -20,7 +66,7 @@ const char * runScript(const char* script, bool isStringBuffer )
 	  luaL_openlibs(L);
 
     DPRINTF("Loading libs... ");
-
+    lua_atpanic(L, test_error);
 	  // init graphics
     luaGraphics_init(L);
     DPRINTF("luaGraphics_init done !\n");
